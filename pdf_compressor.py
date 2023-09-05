ba9be5ed5e5eeaa5bdfbee06bfdd21980d96a423
Icey-Python <65103243+Icey-Python@users.noreply.github.com>
2023-09-05 08:27:53 +0000

    fix(logic): Compressing pdf and optmizing it
    Changed the logic to compress the pdf to a smaller size

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,32 +1,39 @@
-import PyPDF2
-import os
-
+import sys
+from PDFNetPython3 import PDFDoc, Optimizer
 
 def compress_pdf(input_pdf, output_pdf):
-  # Open the PDF files
-  file_size_bytes = os.path.getsize(input_pdf)
-  # Convert to megabytes (MB)
-  file_size_mb = file_size_bytes / 1024 / 1024
-  print("before", file_size_mb)
-  import subprocess
+    try:
+        PDFDoc.Initialize()
+        
+        # Open the input PDF
+        pdf_doc = PDFDoc(input_pdf)
+        
+        # Create a PDF optimizer
+        optimizer = Optimizer()
+        
+        # Set optimization options (e.g., image quality, DPI, etc.)
+        optimizer.SetColorImageSettings(Optimizer.e_jpeg, 10)  # Adjust image quality (0-100)
+        optimizer.SetImageDPI(150)  # Set image DPI
+        
+        # Optimize the PDF
+        optimizer.Optimize(pdf_doc)
+        
+        # Save the compressed PDF to the output file
+        pdf_doc.Save(output_pdf, SDFDoc.e_linearized)
+        
+        print(f"Compression complete. Compressed PDF saved as {output_pdf}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-  # Specify the input and output PDF file paths
+if __name__ == "__main__":
+    
+  # Specify the input and output PDF file names
+  input_pdf = "multi_column_with_long_paragraphs.pdf"
+  output_pdf = "multi_column_with_long_paragraphs.pdf"
 
-  # Run Ghostscript to compress the PDF
-  subprocess.run([
-      "gs", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4",
-      "-dPDFSETTINGS=/ebook", "-dNOPAUSE", "-dBATCH",
-      f"-sOutputFile={output_pdf}", input_pdf
-  ])
-
-  file_size_bytes = os.path.getsize(output_pdf)
-  # Convert to megabytes (MB)
-  file_size_mb = file_size_bytes / 1024 / 1024
-  print("after", file_size_mb)
+    compress_pdf(input_pdf, output_pdf)
 
 
-# Specify the input and output PDF file names
-input_pdf = "multi_column_with_long_paragraphs.pdf"
-output_pdf = "multi_column_with_long_paragraphs.pdf"
+
 
 
